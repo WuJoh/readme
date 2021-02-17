d38e873d0771aca256f649795389f9e111c57b0d
WuJoh <joh.wu@outlook.com>
2021-02-17 10:04:37 -0500

    added questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,52 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: 'input',
+        name: "repo",
+        message: "what is the link of your repository"
+    },
+    {
+        type: 'input',
+        name: "title",
+        message: "What is your Project Title?",
+    },
+    {
+        type: 'input',
+        name: "description",
+        message: "What is your Project Description?",
+    },
+    {
+        type: 'input',
+        name: "installation",
+        message: "What are the steps require to install your project?",
+    },
+    {
+        type: 'input',
+        name: "usage",
+        message: "Provide instructions and examples for use.",
+    },
+    {
+        type: 'input',
+        name: "credits",
+        message: "Provide a list of collaborators with links to Github.",
+    },
+    {
+        type: 'input',
+        name: "license",
+        message: "What are the licenses for your project?",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
